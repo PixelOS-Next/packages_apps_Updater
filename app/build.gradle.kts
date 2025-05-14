703d794c7f264a869b6d452782243f91d734d0bd
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-14 15:56:46 +0545

    Updater: AGP upgrade and migrate to androidx.preference:preference-ktx
    Change-Id: I49be23274c5d46c4f1091c7d115a4397a9efb33d

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
-    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("io.noties.markwon:core:4.6.2")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
